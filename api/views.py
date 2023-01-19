ca5ba0c189442c377680764f7a1d06e5db736f37
Ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-01-19 04:53:12 +0000

    create app api

--- api/views.py
+++ api/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+
+
+from .serializers import *
 
 # Create your views here.
+
+
+class AssistantAPI(APIView):
+    pass
+
+
+class UserAPI(APIView):
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAdminUser]
+
+    def get(self, request, format=None):
+
+        usernames = [user.username for user in User.objects.all()]
+        return Response(usernames)
