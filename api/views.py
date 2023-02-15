fc004866419abaace12662a446c5db4fa63edaab
Ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-02-15 08:04:43 +0000

    change object texttospeech knowledge store in one place

--- api/views.py
+++ api/views.py
@@ -9,34 +9,25 @@ from rest_framework import authentication, permissions
 from utils.AI.Wit import Wit
 from utils.Knowledge.main import Knowlegde
 from utils.TextToSpeech.textToSpeech import textTTS
-
-
-import asyncio
 import json
 
 from .serializers import *
 
 # Create your views here.
 
+knowledge = Knowlegde(config('weatherAPI'), config('rapidAPI'),
+                      config('googleAPI'), config('googleMapAPI'))
+wit = Wit(config('witAPI'))
+textToSpeechURL = textTTS({
+    "Authorization": config('playhtAPIAuthorization'),
+    "X-User-ID": config('playhtAPIX-User-ID')
+})
+
 
 class AssistantAPI(APIView):
     def post(self, request, format=None):
-
-        # if request.data.get('data').get('_streams')[1]:
-        #     # print(request.data['query'])
-        #     # not good to do this
-        #     wit = Wit('Bearer HD7463MS2IMBGP4AW4AFFGTZGVIUF2GI')
-        #     print(request.data['query'])
-        #     intent, confidence, entities = wit.getIntent(
-        #         request.data.get('data').get('_streams')[1])
-        #     print(intent, confidence, entities)
-
-        #     return Response({'intent': intent, 'confidence': confidence, 'entities': entities})
-
         if request.body.decode('utf-8'):
             requestJson = json.loads(request.body.decode('utf-8'))
-
-            wit = Wit(config('witAPI'))
             intent, confidence, entities = wit.getIntent(
                 requestJson.get('query'))
             return Response({'intent': intent, 'confidence': confidence, 'entities': entities})
@@ -48,12 +39,8 @@ class AssistantAPI(APIView):
 
 class KnowledgeAPI(APIView):
     def post(self, request):
-
         if request.body.decode('utf-8'):
-            knowledge = Knowlegde(config('weatherAPI'), config('rapidAPI'),
-                                  config('googleAPI'), config('googleMapAPI'))
             requestJson = json.loads(request.body.decode('utf-8'))
-            # print(type(request.data['query']))
             text = knowledge.findsomething(requestJson.get('question'))
             return Response({'answer': text})
 
@@ -62,17 +49,10 @@ class KnowledgeAPI(APIView):
 
 class TextToSpeech(APIView):
     def post(self, request):
-
         if request.body.decode('utf-8'):
             requestJson = json.loads(request.body.decode('utf-8'))
-            # print(requestJson.get('text'))
-            textToSpeechURL = textTTS({
-                "Authorization": config('playhtAPIAuthorization'),
-                "X-User-ID": config('playhtAPIX-User-ID')
-            })
             print("test1")
             voiceURL = textToSpeechURL.changetextTV(requestJson['text'])
-            # voiceURL = textToSpeechURL.main(requestJson['text'])
             print(voiceURL)
             return Response({'url': voiceURL})
         return Response({'url': None})
@@ -82,8 +62,6 @@ class WeatherAPI(APIView):
     def post(self, request):
         if request.body.decode('utf-8'):
             requestJson = json.loads(request.body.decode('utf-8'))
-            knowledge = Knowlegde(config('weatherAPI'), config('rapidAPI'),
-                                  config('googleAPI'), config('googleMapAPI'))
             text = knowledge.weather(requestJson.get('city'))
 
             return Response({'weather': text})
