ab741fc6dbecd13db41d6ffd498416f7ee7a625c
Ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-02-20 13:55:59 +0000

    clear pyc

--- assistant/urls.py
+++ assistant/urls.py
@@ -15,14 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api-auth/', include('rest_framework.urls')),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
